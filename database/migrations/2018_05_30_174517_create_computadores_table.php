7d909f729accf013afa29328564d4360589316b1
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-05-30 13:10:23 -0500

    Remove files

--- database/migrations/2018_05_30_174517_create_computadores_table.php
+++ database/migrations/2018_05_30_174517_create_computadores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateComputadorsTable extends Migration
+class CreateComputadoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,6 +29,6 @@ class CreateComputadorsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('computadors');
+        Schema::dropIfExists('computadores');
     }
 }
