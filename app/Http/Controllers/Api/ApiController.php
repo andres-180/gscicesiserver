bf07da9b47ab21d1ed977851dd54cb4ff8247037
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-05-30 15:12:59 -0500

    Set dot

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -29,7 +29,7 @@ class ApiController extends Controller
         if (is_null($computador))
         {
             $estado = $computador -> estado;
-            return "el computador si es nullo";
+            return "El estado es: ". $estado;
         }
         else
         {
