1108b88ff179846323e4c44d96431d76b987cf07
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-05-30 11:27:20 -0500

    Add models

--- database/migrations/2018_05_30_155647_create_computadors_table.php
+++ database/migrations/2018_05_30_155647_create_computadors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSalasTable extends Migration
+class CreateComputadorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateSalasTable extends Migration
      */
     public function up()
     {
-        Schema::create('salas', function (Blueprint $table) {
+        Schema::create('computadors', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('sala_id');
+            $table->foreign('sala_id')->references('id')->on('salas');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateSalasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('salas');
+        Schema::dropIfExists('computadors');
     }
 }
