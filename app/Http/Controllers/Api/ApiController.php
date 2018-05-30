010987092ee6b0972c9c029f13359041a6ee8c56
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-05-30 16:00:21 -0500

    set timezone

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -22,14 +22,14 @@ class ApiController extends Controller
         $computador = DB::table('computadores')->where([
             ['sala_id', '=', $idsala],
             ['idComputador', '=', $idpc],
-        ])->update(['last_connection' => $fecha]);
+        ])->update(['last_connection' => $fecha], ['estado' => "disponible"]);
 
         $computador = DB::table('computadores')->where([
             ['sala_id', '=', $idsala],
             ['idComputador', '=', $idpc],
         ])->first();
 
-        return $computador -> last_connection;
+        return "Última conexión: " . $computador -> last_connection;
 
     }
 }
