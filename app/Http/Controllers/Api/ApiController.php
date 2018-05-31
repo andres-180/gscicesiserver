df5a701bdef0c47f838d16cf9cf201bfc97e622b
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-05-30 19:52:26 -0500

    setasdfas

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -45,7 +45,7 @@ class ApiController extends Controller
         {
             $fechaComputador = $computador -> last_connection;
             $diferencia = $fechaActual->diffInSeconds($fechaComputador);
-            if($diferencia > 10)
+            if($diferencia > 15)
             {
                 $computador -> estado = "disponible";
                 $computador -> save();
